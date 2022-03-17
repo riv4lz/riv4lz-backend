38bdc24a081c6a032029ad5f6caf558573d42513
jbn108 <jbuus9@gmail.com>
2022-03-17 11:08:05 +0100

    Usertype Caster created and tested

---
+++ riv4lz-api/riv4lz.core.test/Models/CasterTest.cs
@@ -0,0 +1,54 @@
+using riv4lz.core.Models;
+using Xunit;
+
+namespace riv4lz.core.test.Models;
+
+public class CasterTest
+{
+    private Caster _caster;
+
+    public CasterTest()
+    {
+        _caster = new Caster();
+    }
+    [Fact]
+    public void Caster_CanBeInitialized()
+    {
+        Assert.NotNull(_caster);
+    }
+
+    [Fact]
+    public void Caster_SetId_UpdatesId()
+    {
+        _caster.Id = 1;
+        Assert.Equal(1, _caster.Id);
+    }
+
+    [Fact]
+    public void Caster_SetEmail_UpdatesEmail()
+    {
+        _caster.Email = "test";
+        Assert.Equal("test", _caster.Email);
+    }
+
+    [Fact]
+    public void Caster_SetPassword_UpdatesPassword()
+    {
+        _caster.Password = "test";
+        Assert.Equal("test", _caster.Password);
+    }
+
+    [Fact]
+    public void Caster_SetGamerTag_UpdatesGamerTag()
+    {
+        _caster.GamerTag = "test";
+        Assert.Equal("test", _caster.GamerTag);
+    }
+
+    [Fact]
+    public void Caster_GamerTag_IsString()
+    {
+        _caster.GamerTag = "test";
+        Assert.IsType<string>(_caster.GamerTag);
+    }
+}
---
+++ riv4lz-api/riv4lz.core/Models/Caster.cs
@@ -0,0 +1,6 @@
+namespace riv4lz.core.Models;
+
+public class Caster : BaseUser
+{
+    public string GamerTag { get; set; }
+}
