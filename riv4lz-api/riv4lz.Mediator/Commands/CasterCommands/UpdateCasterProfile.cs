e24a5c14729ec4dbfc8cdd33372cc309ecbf43cb
jbn108 <jbuus9@gmail.com>
2022-04-25 10:20:43 +0200

    update caster profile done

--- riv4lz-api/riv4lz.Mediator/Commands/CasterCommands/UpdateCasterProfile.cs
+++ riv4lz-api/riv4lz.Mediator/Commands/CasterCommands/UpdateCasterProfile.cs
@@ -1,20 +1,44 @@
+using AutoMapper;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
+using riv4lz.dataAccess;
 using riv4lz.Mediator.Dtos;
 
 namespace riv4lz.Mediator.Commands.CasterCommands;
 
 public class UpdateCasterProfile
 {
-    public class Command : IRequest
+    public class Command : IRequest<bool>
     {
         public UpdateCasterProfileDto UpdateCasterProfileDto { get; set; }
     }
     
-    public class Handler : IRequestHandler<Command>
+    public class Handler : IRequestHandler<Command, bool>
     {
-        public Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+        private readonly CasterDbContext _ctx;
+        private readonly IMapper _mapper;
+
+        public Handler(CasterDbContext ctx, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _ctx = ctx;
+            _mapper = mapper;
+        }
+
+        public async Task<bool> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var profile = await _ctx.CasterProfiles.FindAsync(
+                request.UpdateCasterProfileDto.CasterId, cancellationToken);
+            
+            if (profile == null)
+            {
+                return false;
+            }
+            
+            _mapper.Map(request.UpdateCasterProfileDto, profile);
+            
+            var result = await _ctx.SaveChangesAsync(cancellationToken);
+            
+            return result > 0;
         }
     }
 }
--- riv4lz-api/riv4lz.Mediator/Dtos/UpdateCasterProfileDto.cs
+++ riv4lz-api/riv4lz.Mediator/Dtos/UpdateCasterProfileDto.cs
@@ -2,5 +2,5 @@ namespace riv4lz.Mediator.Dtos;
 
 public class UpdateCasterProfileDto
 {
-    
+    public Guid CasterId { get; set; }
 }
