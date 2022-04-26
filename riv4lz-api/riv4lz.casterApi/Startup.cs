6eb60f1c2e55f023fe0016c146f407d4ca18609e
Frann0 <deengo113@gmail.com>
2022-04-26 16:06:55 +0200

    backend for chat
    uden mediator eller noget i den dur, helt clean. Men det virker. Halvt... Som sagt på pr'en på frontend

--- riv4lz-api/riv4lz.casterApi/Startup.cs
+++ riv4lz-api/riv4lz.casterApi/Startup.cs
@@ -69,6 +69,7 @@ namespace riv4lz.casterApi
 
             services.AddMediatR(typeof(CreateUser.Handler).Assembly);
             services.AddAutoMapper(typeof(Startup));
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
