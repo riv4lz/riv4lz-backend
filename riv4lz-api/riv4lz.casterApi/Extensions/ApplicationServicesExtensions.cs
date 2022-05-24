441ccf9cb6e5d9fd17bdda3cc4ba38ef840913c0
jbn108 <jbuus9@gmail.com>
2022-05-24 17:55:52 +0200

    Update ApplicationServicesExtensions.cs

--- riv4lz-api/riv4lz.casterApi/Extensions/ApplicationServicesExtensions.cs
+++ riv4lz-api/riv4lz.casterApi/Extensions/ApplicationServicesExtensions.cs
@@ -35,7 +35,7 @@ namespace riv4lz.casterApi.Extensions
 
             services.AddDbContext<DataContext>(options =>
             {
-                options.UseNpgsql(_configuration.GetConnectionString(_configuration.GetConnectionString("Postgres")));
+                options.UseNpgsql(_configuration.GetConnectionString("Postgres"));
             });
 
             services.AddDbContext<AuthContext>(options =>
