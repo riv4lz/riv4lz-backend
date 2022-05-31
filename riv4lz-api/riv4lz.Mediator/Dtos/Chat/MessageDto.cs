a09d8c8b79bfb0b980ca37e1c6959e6279459f55
jbn108 <jbuus9@gmail.com>
2022-05-31 08:43:19 +0200

    updated messagedto to have userid

--- riv4lz-api/riv4lz.Mediator/Dtos/Chat/MessageDto.cs
+++ riv4lz-api/riv4lz.Mediator/Dtos/Chat/MessageDto.cs
@@ -2,6 +2,7 @@ namespace riv4lz.Mediator.Dtos.Chat;
 
 public class MessageDto
 {
+    public Guid UserId { get; set; }
     public string Username { get; set; }
     public string ProfileImgUrl { get; set; }
     public string Text { get; set; }
